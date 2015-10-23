7777d4eb912b051eacfb7a4aff069e31abbc9ad9
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-10-23 22:48:32 +0200

    Fix dircolors setup

--- dircolors/setup.sh
+++ dircolors/setup.sh
@@ -3,8 +3,9 @@
 mkdir "$HOME/.dircolors"
 cd "$HOME/.dircolors"
 git clone https://github.com/seebi/dircolors-solarized
+chmod +x "$HOME/.dircolors/dircolors-solarized/dircolors.256dark"
 
 echo 'export TERM=xterm-256color' >> "$HOME/.bashrc"
-echo "eval `$HOME/.dircolors/dircolors-solarized/dircolors.256dark`" >> "$HOME/.bashrc"
+echo 'eval `dircolors $HOME/.dircolors/dircolors-solarized/dircolors.256dark`' >> "$HOME/.bashrc"
 
 
