0cf13ea3aa5b819cfa7d8950b273f84643d7e784
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-10-24 16:24:25 +0200

    Don't use backticks

--- dircolors/setup.sh
+++ dircolors/setup.sh
@@ -8,6 +8,6 @@ git clone https://github.com/seebi/dircolors-solarized
 chmod +x "$HOME/.dircolors/dircolors-solarized/dircolors.256dark"
 
 echo 'export TERM=xterm-256color' >> "$HOME/.bashrc"
-echo 'eval `dircolors $HOME/.dircolors/dircolors-solarized/dircolors.256dark`' >> "$HOME/.bashrc"
+echo 'eval "$(dircolors $HOME/.dircolors/dircolors-solarized/dircolors.256dark)"' >> "$HOME/.bashrc"
 
 
